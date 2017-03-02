705105a979efff42789e477468ba9a14ec6613d9
BobWong <wangbo@bluemoon.com.cn>
2017-03-02 10:42:58 +0800

    Upload project

---
+++ iOS/BWiOSEffectiveCoding/BWiOSEffectiveCoding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BWiOSEffectiveCoding
+//
+//  Created by BobWong on 2017/3/2.
+//  Copyright © 2017年 BobWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
